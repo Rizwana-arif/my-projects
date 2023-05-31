103bcea92f417ceef222ceb5c0d602f2e3ce001c
Rizwana-arif <rizwanaarif448@gmail.com>
2023-05-31 07:01:12 -0700

    hello world

--- index-main.php
+++ index-main.php
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-
-$reg=1000;
-$cand1=300;
-$cand2=400;
-$cand3=200;
-$total=$cand1+$cand2+$cand3;
-$totalinterest=$total*100/$reg;
-echo "registered vots: " . $reg;
-echo "<br>";
-echo "candidate1: " . $cand1;
-echo "<br>";
-echo "candidate2: " . $cand2;
-echo "<br>";
-echo "candidate3: " . $cand3;
-echo "<br>";
-echo "total votes caste: " . $total;
-echo "<br>";
-echo "total interest of vote caste: " . $totalinterest;
-echo "<br>";
-$tv=$total;
-echo "total reserved votes: " . $tv;
-$cand1int=$cand1*100/$tv;
-$cand2int=$cand2*100/$tv;
-$cand3int=$cand3*100/$tv;
-echo "<br>";
-echo "candidate1 interest: " . $cand1int;
-echo "<br>";
-echo "candidate2 interest: " . $cand2int;
-echo "<br>";
-echo "candidate3 interest: " . $cand3int;
-
-
-
-
-
-?>
-=======
 <?php 
 include ("./include/header.php");
 include ("./include/sidebar.php");
@@ -757,4 +717,4 @@ include ("./include/connection.php");
         </div>
       </div>
 <?php include ("./include/footer.php"); ?>
->>>>>>> 63ece8c (hello world)
+
